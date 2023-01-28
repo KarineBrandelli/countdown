80a42976ea4bbaf1763b7166e06942fe5ff944d1
Karine Brandelli <karinebrandelli@live.com>
2023-01-28 15:12:42 -0300

    build: add main.js file

---
+++ src/main.js
@@ -0,0 +1,31 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+const myBday = "25 May 2023";
+
+function countdown() {
+  const myBdayDate = new Date(myBday);
+  const currentDate = new Date();
+
+  const totalSeconds = (myBdayDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
